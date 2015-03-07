556e10cac2a9746dc1a2993026c60116f893fb18
Binoy T V <binoytv9@gmail.com>
2015-03-07 21:00:35 +0530

    initial commit

---
+++ basic-synchronization-patterns/barrier.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+
+#define n 10    // number of threads
+
+int count;
+sem_t mutex, barrier;
+
+void *thread(void *a)
+{
+    sem_wait(&mutex);
+    count += 1;
+    sem_post(&mutex);
+
+    if(count == n)
+        sem_post(&barrier);
+
+    sem_wait(&barrier);
+    sem_post(&barrier);
+
+    printf("thread%d critical section\n", *(int *)a);
+}
+
+main()
+{
+    int i;
+    sem_init(&mutex, 0, 1);
+    sem_init(&barrier, 0, 0);
+
+    pthread_t t[n];
+
+    for(i = 0; i < n; ++i)
+        pthread_create(&t[i], 0, thread, &i);
+
+    for(i = 0; i < n; ++i)
+        pthread_join(t[i], 0);
+    printf("count is %d\n", count);
+}
--- basic-synchronization-patterns/multiplex.c
+++ basic-synchronization-patterns/multiplex.c
@@ -2,60 +2,37 @@
 #include<pthread.h>
 #include<semaphore.h>
 
-#define N 10 // maximum number of concurrent threads
+#define N 50 // maximum number of threads
+#define n 10 // maximum number of concurrent threads
 
 int count;
 sem_t multiplex;
 
-void *thread1(void *a)
-{
-    sem_wait(&multiplex);
-    // critical section
-    count += 1;
-    sem_post(&multiplex);
-}
-
-void *thread2(void *a)
-{
-    sem_wait(&multiplex);
-    // critical section
-    count += 1;
-    sem_post(&multiplex);
-}
-.
-.
-.
 void *threadN(void *a)
 {
     sem_wait(&multiplex);
     // critical section
+    int sval;
+    sem_getvalue(&multiplex, &sval);
+
+    printf("thread%d semVal : %d\n", *(int *)a, sval);
     count += 1;
     sem_post(&multiplex);
 }
 
-
 main()
 {
-    pthread_t t1, t2, ... tN;
-    sem_init(&multiplex, 0, N);
+    int i;
+    pthread_t t[N];
+    sem_init(&multiplex, 0, n);
 
     printf("count before : %d\n", count);
 
-
-    pthread_create(&t1, 0, thread1, 0);
-    pthread_create(&t2, 0, thread2, 0);
-    .
-    .
-    .
-    pthread_create(&tN, 0, threadN, 0);
-
-
-    pthread_join(t1, 0);
-    pthread_join(t2, 0);
-    .
-    .
-    .
-    pthread_join(tN, 0);
+    for(i = 0; i < N; ++i)
+        pthread_create(&t[i], 0, threadN, &i);
+    
+    for(i = 0; i < N; ++i)
+        pthread_join(t[i], 0);
 
 
     printf("count after : %d\n", count);
